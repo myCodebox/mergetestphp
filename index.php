e4d7337a0b2c297544b980c20e8b0ee014cd0d41
Spelunky <myCodebox@users.noreply.github.com>
2022-12-02 20:46:34 +0100

    Test

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
 
-<<<<<<< HEAD
 function writeMsg2() {
     echo "Hello Test!";
 }
     
 writeMsg2();
-=======
-function testfunc2() {
-    return 'test';
-}
-
-echo testfunc2();
-
->>>>>>> cb40b7e880113447f62a64a7f42ed6df83482d08
