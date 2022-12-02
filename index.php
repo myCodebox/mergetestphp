cb40b7e880113447f62a64a7f42ed6df83482d08
Spelunky <myCodebox@users.noreply.github.com>
2022-12-02 20:17:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,8 @@
 <?php
 
-function testfunc6() {
-    return 'test';
-}
-
-
-function writeMsg22() {
-    echo "Hello Test!";
-}
-
-function testfunc() {
-    return 'test';
-}
 function testfunc2() {
     return 'test';
 }
-    
-writeMsg2(); // call the function
-echo 'Hello';
+
 echo testfunc2();
 
